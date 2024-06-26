3bc015b9b7b06dd49dcca6a1ccd599e4d7652399
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-06-26 23:42:18 +0300

    0x01-ES6_promise - Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    return results.map((res) => ({
+      status: res.status,
+      value: res.value || res.reason,
+    }));
+  });
+}
+
+export default handleProfileSignup;
