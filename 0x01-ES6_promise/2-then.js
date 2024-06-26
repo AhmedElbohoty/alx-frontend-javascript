0f7011db590a2f567e59d2f2f9331c9d4fb82bf4
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-06-26 22:36:07 +0300

    0x01-ES6_promise - Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+function handleResponseFromAPI(promise) {
+  const data = { status: 200, body: 'success' };
+  return promise
+    .then(() => data)
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
