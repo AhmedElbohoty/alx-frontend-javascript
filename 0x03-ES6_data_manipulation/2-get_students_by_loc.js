f575bd4c4de3d4cc8995a72e1df8fb8b536f5c25
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-07-02 12:16:34 +0300

    Fix bug

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
 export function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) return [];
+  if (typeof city !== 'string') return [];
 
   return students.filter((st) => st.location === city);
 }
