37d45ca1bbc55ee3931fe267e27904b022015e21
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-07-03 21:46:19 +0300

    0x04-TypeScript - task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,47 @@ interface Teacher {
   [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentClassConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Examples
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -16,3 +57,9 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+console.log(printTeacher('John', 'Doe'));
+
+const student = new StudentClass({ firstName: 'Ahmed', lastName: 'Elbohoty' });
+console.log(student.workOnHomework());
+console.log(student.displayName());
